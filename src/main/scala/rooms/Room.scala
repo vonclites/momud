0f4549e106ecfd08e5c345fc6bd5b1857ac37a66
vonclites <alamos52@gmail.com>
2014-11-25 21:45:29 -0500

    Rooms can facilitate movement of users.

--- src/main/scala/rooms/Room.scala
+++ src/main/scala/rooms/Room.scala
@@ -5,6 +5,7 @@ import akka.actor._
 object Room {
 	case class SetExits(exits: Map[String,(String,ActorRef)])
 	case class Arrive(name: String, ref: ActorRef)
+	case class Depart(name: String, dir: String)
 	case class GetUsers()
 	def props(id: Int, name: String, desc: String) = Props(classOf[Room], id, name, desc)
 }
@@ -16,6 +17,13 @@ class Room(val id: Int, val name: String, val desc: String) extends Actor{
 	def receive = {
 		case SetExits(exits) => this.exits = exits
 		case Arrive(name, ref) => users = users + ((name,ref))
+		case Depart(name, dir) => {
+			if (exits.isDefinedAt(dir)){
+			users = users - name
+			exits(dir)._2 ! Arrive(name, sender)
+			//TODO::Notify users of departure
+			} else sender ! "Not an exit" //TODO::Integrate with User interface
+		} 
 		case GetUsers => sender ! users
 	}
 }
